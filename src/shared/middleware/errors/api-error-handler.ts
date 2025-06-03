e235c8f44f2d6ffb8dadbea59813d6c8b78fcdf5
ogagaoghene emmanuel <esavwede84@gmail.com>
2025-06-03 09:27:02 +0100

    feat: added api error response

--- src/shared/middleware/errors/api-error-handler.ts
+++ src/shared/middleware/errors/api-error-handler.ts
@@ -1,5 +1,4 @@
 import { Request, Response, NextFunction } from "express"
-import crypto from "crypto"
 import ApiError from "./api-error"
 import logger from "../../../core/logging/logger"
 
@@ -13,17 +12,18 @@ export default function apiErrorHandler(
   const e = error as ApiError
 
   switch (e.statusCode) {
+    case 400:
+      return res.status(400).json({
+        status: "error",
+        error: { message: e.message, code: e.code },
+      })
+
     case 500:
-      // const errorMetadata = {
-      //   status: "error",
-      //   code: e.statusCode,
-      //   message: "Internal Server Error",
-      //   correlationId: req.get("X-Correlation-ID") || crypto.randomUUID(),
-      // }
+      return res.status(500).json({
+        status: "error",
+        error: { message: e.message, code: e.code },
+      })
 
-      logger.error(e.message, { ...e.data })
-
-      return res.status(500).json({ success: false, msg: "server error" })
     default:
       logger.warn("UNKNOWN_SERVER_ERROR", e.data)
       console.error(e)
--- src/shared/middleware/errors/api-error.ts
+++ src/shared/middleware/errors/api-error.ts
@@ -1,11 +1,13 @@
 export default class ApiError extends Error {
   public statusCode: number
   public data: any
+  public code: string
 
-  constructor(message: string, statusCode: number) {
+  constructor(statusCode: number, code: string, message: string) {
     super(message)
     Object.setPrototypeOf(this, ApiError.prototype)
     this.data = {}
     this.statusCode = statusCode
+    this.code = code
   }
 }
