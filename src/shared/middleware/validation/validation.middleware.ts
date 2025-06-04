0accfe593113ce5a9ccfe672c40042393d8f2b75
ogagaoghene emmanuel <esavwede84@gmail.com>
2025-06-04 14:25:20 +0100

    fix: improved schema validation

--- src/shared/middleware/validation/validation.middleware.ts
+++ src/shared/middleware/validation/validation.middleware.ts
@@ -1,43 +1,65 @@
-import { ZodSchema } from "zod"
+import { ZodError, ZodSchema } from "zod"
 import { Request, Response, NextFunction } from "express"
 
-export default async function validateRequestSchema(schemas: {
+export default function validateRequestSchema(schemas: {
   body?: ZodSchema
   params?: ZodSchema
   query?: ZodSchema
 }) {
-  return function (req: Request, res: Response, next: NextFunction) {
+  return async (req: Request, res: Response, next: NextFunction) => {
     // set errors variable
-    var validationErrors = []
+    var validationErrors: any = []
 
     if (schemas.body) {
-      // validate
       try {
+        req.body = await schemas.body.parseAsync(req.body)
       } catch (e: any) {
-        // add to errors
+        if (e instanceof ZodError) {
+          e.issues.forEach((issue) => {
+            validationErrors.push(issue)
+          })
+        }
       }
     }
 
     if (schemas.params) {
       try {
         // validate
+        req.params = await schemas.params.parseAsync(req.params)
       } catch (e: any) {
-        // add to errors
+        if (e instanceof ZodError) {
+          e.issues.forEach((issue) => {
+            validationErrors.push(issue)
+          })
+        }
       }
     }
 
     if (schemas.query) {
       try {
         // validate
+        req.query = await schemas.query.parseAsync(req.query)
       } catch (e: any) {
-        // add to errors
+        if (e instanceof ZodError) {
+          e.issues.forEach((issue) => {
+            validationErrors.push(issue)
+          })
+        }
       }
     }
 
     if (validationErrors.length > 0) {
       // throw 400 error
+      console.dir("Validation errors:", validationErrors)
+      res.status(400).json({
+        status: "error",
+        message: "Validation failed",
+        errors: validationErrors,
+      })
+      return
     }
 
     // call next middleware
+    next()
   }
 }
