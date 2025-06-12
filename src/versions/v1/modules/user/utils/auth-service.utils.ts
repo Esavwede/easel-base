95f51f40ea56f2dd1823045752ba24bfe6ce7554
ogagaoghene emmanuel <esavwede84@gmail.com>
2025-06-12 13:04:13 +0100

    chore: remove unecessary number import

--- src/versions/v1/modules/user/utils/auth-service.utils.ts
+++ src/versions/v1/modules/user/utils/auth-service.utils.ts
@@ -1,9 +1,31 @@
 import { redis } from "../../../../../core/cache/redis-server"
 
 export default class AuthService {
+  // Properties
+  private static readonly MAX_SIGNIN_ATTEMPTS = 3
+
   static async checkAccountLock(userId: string): Promise<boolean> {
     const lockKey = `account_lock:${userId}`
     const isLocked = await redis?.get(lockKey)
     return !!isLocked
+  }
+
+  static async incrementSigninAttempts(userId: string): Promise<void> {
+    const attemptsKey = `login_attempts:${userId}`
+    const signinAttempts: number | undefined = await redis?.incr(attemptsKey)
+
+    //*** Buggy feature */
+    if (typeof signinAttempts !== "number") {
+      return
+    }
+
+    if (signinAttempts === 1) {
+      await redis?.expire(attemptsKey, 900)
+    }
+
+    if (signinAttempts > this.MAX_SIGNIN_ATTEMPTS) {
+      const lockKey = `account_lock:${userId}`
+      await redis?.set(lockKey, "locked", "EX", 900)
+    }
   }
 }
